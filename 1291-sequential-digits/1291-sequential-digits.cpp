b502ba88bc646e2aae76ed030c601539c8ab0553
Poojan Bhardwaj <poojanbhardwaj2008@gmail.com>
2026-07-13 23:42:42 +0530

    Time: 1 ms (7.62%), Space: 8.1 MB (97.69%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,26 +1,29 @@
+#include <vector>
+
 class Solution {
 public:
-    inline static int q[45];
-    inline static bool init = []() {
-        int n = 0;
-        for (int i = 1; i < 10; i++)
-            q[n++] = i;
+    auto sequentialDigits(int low, int high) -> std::vector<int> {
+        std::vector<int> result;
 
-        for (int i = 0; i < n; i++) {
-            int d = q[i] % 10;
-            if (d < 9) q[n++] = q[i] * 10 + d + 1;
+        for (int length = 2; length <= 9; ++length) {
+            for (int first = 1; first + length <= 10; ++first) {
+                int value = 0;
+
+                for (int digit = first; digit < first + length; ++digit) {
+                    value = value * 10 + digit;
+                }
+
+                if (value >= low && value <= high) {
+                    result.push_back(value);
+                }
+            }
+
+
+
+
+            
         }
 
-        return 0;
-    }();
-
-    vector<int> sequentialDigits(int low, int high) {
-        vector<int> res;
-
-        for (auto& x : q)
-            if (x >= low && x <= high)
-                res.push_back(x);
-
-        return res;
+        return result;
     }
 };
