c57f46b76bb4f7ff3f723ab0fe34dee6fb216cb2
Poojan Bhardwaj <poojanbhardwaj2008@gmail.com>
2026-07-15 22:55:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -3,10 +3,7 @@ public:
     int gcdOfOddEvenSums(int n) {
         int so = (n*n);
         int se = (n)*(n+1);
-        int ans = 1;
-        for(int i = min(so,se);i>0;i--){
-            if(so%i == 0 && se%i == 0) return i;
-        }
-        return 0;
+       
+          return __gcd(so,se);
     }
 };
