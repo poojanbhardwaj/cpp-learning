3cc2212830491cf8d6d1032c4b126b6c90e1c4b2
Poojan Bhardwaj <poojanbhardwaj2008@gmail.com>
2026-08-26 08:21:48 +0530

    Time: 54 ms (49.73%), Space: 54.8 MB (13.62%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    bool f(string s,string p,int n,int m,vector<vector<int>> &dp){
-        if(n<0 && m<0){
-            return true;
-        }
-        if(m<0){
-            return false;
-        }
-        if(n < 0){
-            for(int i = 0;i<=m;i++){
-                if(p[i] != '*') return false;
-            }
-            return true;
-        }
-        if(dp[n][m] != -1) return dp[n][m];
-        if(s[n] == p[m] || p[m] == '?'){
-            return dp[n][m] = f(s,p,n-1,m-1,dp);
-        }
-        else if(p[m] == '*'){
-            return dp[n][m] = f(s,p,n,m-1,dp)||f(s,p,n-1,m,dp);
-        }
-        return dp[n][m] = false;
-      
-    }
+
     bool isMatch(string s, string p) {
         int n= s.size();
         int m = p.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return f(s,p,n-1,m-1,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        dp[0][0] = true;
+        for(int i = 1;i<=n;i++){
+            dp[i][0] = false;
+        }
+        for(int j = 1;j<=m;j++){
+            if(p[j-1]=='*' && dp[0][j-1]){
+                dp[0][j] = true;
+            }
+            else dp[0][j] = false;
+        }
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1] == '*'){
+                    dp[i][j] = dp[i-1][j]||dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
