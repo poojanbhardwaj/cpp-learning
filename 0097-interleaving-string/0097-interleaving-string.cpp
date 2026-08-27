2fdac0de3059e2526961c5e46d17ac2c6ed45d51
Poojan Bhardwaj <poojanbhardwaj2008@gmail.com>
2026-08-27 06:20:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (33.12%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
-    bool f(string &s,string &p,string &t,int i,int j,vector<vector<int>> &dp){
+    // bool f(string &s,string &p,string &t,int i,int j,vector<vector<int>> &dp){
         
         
-        if(i == 0){
-            for(int k = 0;k<j;k++){
-                if(p[k] != t[k]) return false;
-            }
-            return true;
-        }
-         if(j == 0){
-            for(int k = 0;k<i;k++){
-                if(s[k] != t[k]) return false;
-            }
-            return true;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        bool ans = false;
-        if(s[i-1] == t[i+j-1]){
-           ans = f(s,p,t,i-1,j,dp);
-        }
-        if(p[j-1] == t[i+j-1]) ans = ans||f(s,p,t,i,j-1,dp);
-        return dp[i][j] = ans;
+    //     if(i == 0){
+    //         for(int k = 0;k<j;k++){
+    //             if(p[k] != t[k]) return false;
+    //         }
+    //         return true;
+    //     }
+    //      if(j == 0){
+    //         for(int k = 0;k<i;k++){
+    //             if(s[k] != t[k]) return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     bool ans = false;
+    //     if(s[i-1] == t[i+j-1]){
+    //        ans = f(s,p,t,i-1,j,dp);
+    //     }
+    //     if(p[j-1] == t[i+j-1]) ans = ans||f(s,p,t,i,j-1,dp);
+    //     return dp[i][j] = ans;
 
-    }
-    bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-      
-        if(n+m != s3.size()) return false;
-        return f(s1,s2,s3,n,m,dp);
+    // }
+    bool isInterleave(string s, string p, string t) {
+        int n = s.size();
+        int m = p.size();
+        
+        if(n+m != t.size()) return false;
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        dp[0][0] = true;
+        for(int i = 1;i<=n;i++){
+            
+                if(s[i-1] == t[i-1] && dp[i-1][0]) dp[i][0]= true;
+            
+        }
+        for(int j = 1;j<=m;j++){
+            if(p[j-1] == t[j-1] && dp[0][j-1])dp[0][j] = true;
+        }
+        
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                bool ans = false;
+                if(s[i-1] == t[i+j-1]) {
+                   ans = dp[i-1][j];
+                }
+                if(p[j-1] == t[i+j-1]){
+                    ans = ans||dp[i][j-1];
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[n][m];
     }
 };
