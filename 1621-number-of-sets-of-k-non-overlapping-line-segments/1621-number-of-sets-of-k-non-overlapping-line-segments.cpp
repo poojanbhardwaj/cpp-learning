e1de4dbb92743ad91e1435b03a7b7e5552b6adb8
Poojan Bhardwaj <poojanbhardwaj2008@gmail.com>
2026-09-16 22:45:32 +0530

    Time: 41 ms (59.35%), Space: 87.5 MB (25.7%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-int mod = 1e9+7;
-    int f(int i,int k,int n,vector<vector<int>> &dp){
-        if(k == 0) return 1;
-        if(i == n) return 0;
-        if(dp[i][k] != -1) return dp[i][k];
-        int skip = f(i+1,k,n,dp)%mod;
-        int take = 0;
-        for(int j = i+1;j<=n-1;j++){
-            take+=f(j,k-1,n,dp)%mod;
-        }
-        return dp[i][k]=(take+skip)%mod;
-    }
-    int numberOfSets(int n, int k) {
-        vector<vector<int>> dp(n+1,vector<int> (k+1,0));
-        vector<int> pref(k+1,0);
-        for(int i = 0;i<n;i++) dp[i][0] = 1;
-        for(int i = n-1;i>=0;i--){
+    static constexpr int MOD = 1'000'000'007;
+    vector<vector<int>> dp;
 
-            for(int j = 1;j<=k;j++){
-                int skip = dp[i+1][j]%mod;
-                int take = 0;
-                
-                    take = pref[j-1]%mod;
-                
-                dp[i][j] = (skip+take)%mod;
-            }
-            for(int j = 0;j<=k;j++){
-                pref[j] = (pref[j] + dp[i][j])%mod;
-            }
-            
-        }
-        return dp[0][k]%mod;
+    int f(int n, int r) {
+        if (r == 0) return 1;
+        if (r > n) return 0;
+
+        int& ans = dp[n][r];
+        if (ans != -1) return ans;
+
+        int take = f(n - 1, r - 1);
+        int skip = f(n - 1, r);
+
+        return ans = (take + skip) % MOD;
+    }
+
+    int numberOfSets(int n, int k) {
+        int positions = n + k - 1;
+        int endpoints = 2 * k;
+
+        dp.assign(positions + 1, vector<int>(endpoints + 1, -1));
+
+        return f(positions, endpoints);
     }
 };
